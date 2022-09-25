aa0efb08652c0cef527bc33f972ddb13935a32b5
pardhu <pardhasai97@gmail.com>
2022-09-25 21:27:41 +0530

    api key change, added score component

--- src/Common/Constants/Constants.js
+++ src/Common/Constants/Constants.js
@@ -1,4 +1,4 @@
-export const API_KEY = '?apikey=37be7389-ad00-4ee9-a260-b1180d227dbe';
+export const API_KEY = '?apikey=550f967a-eb14-4439-a1c8-275198219d29';
 export const API_ENDPOINT = 'https://api.cricapi.com/v1/';
 export const CURRENT_MACTCHES = 'currentMatches';
 export const UPCOMING_MACTCHES = 'cricScore';
